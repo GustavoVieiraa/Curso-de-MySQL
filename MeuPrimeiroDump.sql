f851a72af09f6176b9d8b8183a4e426ae3351e4b
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-17 21:44:17 -0300

    aula08 e primeiro DUMP concluído e executados com sucesso!

---
+++ MeuPrimeiroDump.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cadastro`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	5.7.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `idcurso` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `descricao` text,
+  `carga` int(10) unsigned DEFAULT NULL,
+  `totaulas` int(10) unsigned DEFAULT NULL,
+  `ano` year(4) DEFAULT '2022',
+  PRIMARY KEY (`idcurso`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'HTML5','Curso sobre TAGS HTML',120,40,2022),(2,'CSS3','Curso de Style em CSS',60,25,2022),(3,'PHP','Curso de PHP POO',180,65,2022),(4,'JavaScript','Curso de Interatividade com JavaScript',90,20,2022),(5,'Java','Curso de Programacao utilizando Java',10,5,2022),(6,'Word','Curso de Word no pacote Office',90,18,2022),(7,'Redes de Computadores','Curso completo sobre redes de computadores',120,32,2022),(8,'Unity','Aprendendo a criar jogos em Unity',350,120,2022);
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jogadores`
+--
+
+DROP TABLE IF EXISTS `jogadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jogadores` (
+  `beta` varchar(20) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `nascimento` date DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `vip` enum('V','P') NOT NULL DEFAULT 'P',
+  `peso` decimal(5,2) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `nacionalidade` varchar(20) DEFAULT 'Brasil',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jogadores`
+--
+
+LOCK TABLES `jogadores` WRITE;
+/*!40000 ALTER TABLE `jogadores` DISABLE KEYS */;
+INSERT INTO `jogadores` VALUES (NULL,1,'Gustavo','2003-02-24','M','P',78.50,1.73,'Brasil'),(NULL,2,'Ana','2003-01-12','F','P',65.20,1.65,'Brasil'),(NULL,3,'André','2000-10-22','M','P',95.20,1.85,'Estados Unidos'),(NULL,4,'Gabriela','2005-09-17','F','P',59.50,1.68,'Portugal'),(NULL,5,'Moises','1999-04-09','M','P',100.50,1.82,'Brasil'),(NULL,6,'Marcelo','2001-01-14','M','P',87.00,1.74,'Brasil'),(NULL,7,'Paulo','1987-08-09','M','P',92.50,1.90,'Estados Unidos');
+/*!40000 ALTER TABLE `jogadores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-17 21:30:14
